7c54d4457451cc9c4c56672aa19b535dba6aec92
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-03-27 09:38:36 +0900

    Update 2-3.c

---
+++ code/2-3/2-3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include
+
+int main() {
+  char a[64] = "Hello";
+  char b[64] = "World";
+  char c[128];
+
+  printf("length of a: %d\n", len);
+
+  printf("a: %s, c: %s\n", a, c);
+
+  printf("c: %s\n", c);
+
+  printf("strcmp(%s, %s) = %d\n", a, a, );
+  printf("strcmp(%s, %s) = %d/n", a, b, );
+
+  return 0;
+}
