5fd89caf75203d85764c12fe806259ea98d4459d
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-03-27 09:48:23 +0900

    Update 2-5.c

---
+++ code/2-5/2-5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include
+
+int main() {
+  double x = 9.0;
+  double y = 2.5;
+
+  /* sqrt: 제곱근 */
+  printf(%.2f\n",  (x));
+  /* pow: 거듭제곱 */
+  printf("%.2f\n",  (x, y));
+  /* ceil: 올림 */
+  printf("%.1f\n",  (y));
+  /* floor: 내림 */
+  printf("%.1f\n",  (y));
+
+  return 0;
+}
