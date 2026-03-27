2667101f40b13c9e69fe4e9db3b438a98edf28cb
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-03-27 10:21:40 +0900

    Update 4-2.c

---
+++ code/4-2/4-2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int sum(int arr[], int n) {
+
+}
+
+double average(int arr[], int n) {
+
+}
+
+int max(int arr[] int n) {
+
+}
+
+int main() {
+  int arr[] = {3, 7, 1, 9, 4, 6};
+  int n = sizeof(arr) / sizeof(arr[0]);
+
+  printf("sum: %d\n", sum(arr, n));
+  printf("avvg: %.2f\n", avearge(arrm n));
+  printf("max: %d\n", max(arr, n));
+  return 0;
+}
