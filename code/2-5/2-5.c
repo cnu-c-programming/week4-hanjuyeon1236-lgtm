54e88778f47e423f96197c1d5bba180ad4083db5
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-04-01 00:39:54 +0900

    Update 2-5.c

--- code/2-5/2-5.c
+++ code/2-5/2-5.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-#include
+#include <math.h>
 
 int main() {
   double x = 9.0;
   double y = 2.5;
 
   /* sqrt: 제곱근 */
-  printf(%.2f\n",  (x));
+  printf(%.2f\n", sqrt(x));
   /* pow: 거듭제곱 */
-  printf("%.2f\n",  (x, y));
+  printf("%.2f\n", pow(x, y));
   /* ceil: 올림 */
-  printf("%.1f\n",  (y));
+  printf("%.1f\n", ceil(y));
   /* floor: 내림 */
-  printf("%.1f\n",  (y));
+  printf("%.1f\n", floor(y));
 
   return 0;
 }
