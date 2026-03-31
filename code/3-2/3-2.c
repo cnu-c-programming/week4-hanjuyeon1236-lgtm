41a5040c71abc2ebf3deca90484ebefba04ec9dd
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-04-01 00:44:16 +0900

    Update 3-2.c

--- code/3-2/3-2.c
+++ code/3-2/3-2.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 void swqp_endian(int *x) {
+  unsigned int val = (unsigned int)*x;
+  *x = ((val & 0x000000FF) << 24)
+       ((val & 0x0000FF00) << 8)
+       ((val & 0x00FF0000) >> 8)
+       ((val & 0xFF000000) >> 24);
 
 }
 
