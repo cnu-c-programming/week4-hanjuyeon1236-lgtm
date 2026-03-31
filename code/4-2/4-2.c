745fe5ff45f418839d05382f43aad9b22fc8ea1f
hanjuyeon1236-lgtm <hanjuyeon1236@gmail.com>
2026-04-01 01:15:24 +0900

    Update 4-2.c

--- code/4-2/4-2.c
+++ code/4-2/4-2.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
 
 int sum(int arr[], int n) {
-
+  int s = 0;
+  for(int i = 0; i < n; i++) {
+    s += arr[i];
+  }
+  return s;
 }
 
 double average(int arr[], int n) {
-
+  if(n == 0) return 0.0;
+  return (double)sum(arr, n) / n;
 }
 
 int max(int arr[] int n) {
-
+  if(n <= 0) return 0;
+  int m = arr[0];
+  for(int i = 1; i < n; i++) {
+    if(arr[i] > m) {
+      m = arr[i];
+    }
+}
+return m;
 }
 
 int main() {
@@ -17,7 +29,7 @@ int main() {
   int n = sizeof(arr) / sizeof(arr[0]);
 
   printf("sum: %d\n", sum(arr, n));
-  printf("avvg: %.2f\n", avearge(arrm n));
+  printf("avg: %.2f\n", avearge(arrm n));
   printf("max: %d\n", max(arr, n));
   return 0;
 }
